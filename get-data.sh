7543871452a7afbf2474eeea7f3ab0db5c750348
Xin Meng <x.meng@outlook.com>
2018-09-07 16:26:31 +0100

    Why: update to 2.6.1 *
    This change addresses the need by:
    *
    
    [issue: X]

--- get-data.sh
+++ get-data.sh
@@ -1,5 +1,5 @@
 #! bin/sh
-LEANOTE_VERSION=2.6
+LEANOTE_VERSION=2.6.1
 wget https://jaist.dl.sourceforge.net/project/leanote-bin/${LEANOTE_VERSION}/leanote-linux-amd64-v${LEANOTE_VERSION}.bin.tar.gz
 tar zxf leanote-linux-amd64-v${LEANOTE_VERSION}.bin.tar.gz
 mv leanote/mongodb_backup .
